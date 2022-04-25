8b1ed2c2f85c638e7768626e75e77d30bec39905
habib-sc <habib97.cs@gmail.com>
2022-04-25 19:43:06 +0600

    if token not valid then signout and navigate to logout, used axios interceptors

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,23 +1,31 @@
-import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../api/axiosPrivate';
 import auth from '../../firebase.init';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
 
     useEffect( () => {
         
         const getOrders = async() => {
             const email = user?.email;
             const url = `http://localhost:5000/orders?email=${email}`;
-            await axios.get(url, {
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            try{
+                await axiosPrivate.get(url)
+                .then(res => setOrders(res.data));
+            }
+            catch(error){
+                console.log(error.message);
+                if (error.response.status === 401 || error.response.status === 403) {
+                    signOut(auth);
+                    navigate('/login');
                 }
-            })
-            .then(res => setOrders(res.data));
+            }
         }
         getOrders();
     }, []);
